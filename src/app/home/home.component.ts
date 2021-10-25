ea267e507bbe9bd235ef773c3d144322d8793a91
JuneeDinh <dinhttnhung9910@gmail.com>
2021-10-25 23:14:09 +0700

    don't know the problem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  longText='Lorem Ipsum is simply dummy text of the printing';
+
   constructor() { }
 
   ngOnInit(): void {}
